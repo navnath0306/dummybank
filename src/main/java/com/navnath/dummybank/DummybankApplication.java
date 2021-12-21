87aa18aeaacf68a62f5cab407af55872ff7160d2
NAVNATH KADAM <kadamnavnath20@gmail.com>
2021-12-21 16:24:14 +0530

    Project skeleton

---
+++ src/main/java/com/navnath/dummybank/DummybankApplication.java
@@ -0,0 +1,13 @@
+package com.navnath.dummybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummybankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummybankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/navnath/dummybank/DummybankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navnath.dummybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummybankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
